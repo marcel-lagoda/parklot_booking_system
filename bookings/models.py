a4ecfc7955c9e4eb80f06ea3e2e72555c5e70cbd
Marcel <marceli.lagoda@gmail.com>
2020-03-15 14:56:51 +0100

    Create booking db model

--- bookings/models.py
+++ bookings/models.py
@@ -1,3 +1,19 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
-# Create your models here.
+
+class Booking(models.Model):
+    NEW = 'NEW'
+    CANCELLED = 'CANCELLED'
+
+    STATUS_CHOICE = [
+        (NEW, 'NEW'),
+        (CANCELLED, 'CANCELLED'),
+    ]
+
+    number = models.PositiveIntegerField()
+    arrival = models.DateField()
+    departure = models.DateField()
+    no_of_people = models.PositiveSmallIntegerField()
+    status = models.CharField(max_length=16, choices=STATUS_CHOICE)
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
