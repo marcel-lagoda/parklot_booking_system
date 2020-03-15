3e364fd426419ccbe22a1fcf6c040fd6cd2b7e7d
Marcel <marceli.lagoda@gmail.com>
2020-03-15 22:07:51 +0100

    Update test for accounts

--- accounts/tests.py
+++ accounts/tests.py
@@ -8,11 +8,9 @@ class CustomUserTest(TestCase):
         User = get_user_model()
 
         user = User.objects.create_user(
-            username='marcel',
             email='marcel@marcel.com',
             password='testpassword',
         )
-        self.assertEqual(user.username, 'marcel')
         self.assertEqual(user.email, 'marcel@marcel.com')
         self.assertTrue(user.is_active)
         self.assertFalse(user.is_superuser)
