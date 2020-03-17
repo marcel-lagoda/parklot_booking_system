d77c0e222bbff7c9d5af6f5cf5db60baba2ef86b
Marcel <marceli.lagoda@gmail.com>
2020-03-17 02:46:16 +0100

    Update Booking db model

--- bookings/models.py
+++ bookings/models.py
@@ -11,9 +11,9 @@ class Booking(models.Model):
         (CANCELLED, 'CANCELLED'),
     ]
 
-    number = models.PositiveIntegerField()
+    number = models.PositiveIntegerField(unique=True)
     arrival = models.DateField()
     departure = models.DateField()
-    no_of_people = models.PositiveSmallIntegerField()
+    no_of_people = models.PositiveSmallIntegerField(default=1)
     status = models.CharField(max_length=16, choices=STATUS_CHOICE)
-    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, related_name='users')
