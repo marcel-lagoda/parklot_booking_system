9b99f4b0ba56d4f216400363d2b9eb6e79deeb6e
Marcel <marceli.lagoda@gmail.com>
2020-03-18 01:20:33 +0100

    Write initial HomePageView

--- bookings/views.py
+++ bookings/views.py
@@ -1,3 +1,16 @@
+from django.contrib.auth import get_user_model
 from django.shortcuts import render
+from django.views import View
+from django.views.generic import ListView
 
-# Create your views here.
+
+from .models import Booking
+
+
+CustomUser = get_user_model()
+
+
+class HomePageView(ListView):
+    model = Booking
+    template_name = 'home.html'
+    context_object_name = 'bookings'
