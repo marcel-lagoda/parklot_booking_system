3306cd04e72e4a45cd06d3a51bb5d7924cefbde7
Marcel <marceli.lagoda@gmail.com>
2020-03-19 01:03:14 +0100

    Update bookings urls

--- bookings/urls.py
+++ bookings/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.HomePageView.as_view(), name='home'),
+    path('update/<int:pk>/', views.UpdateRezView.as_view(), name='update'),
+    path('booking/<int:pk>/', views.DetailRezView.as_view(), name='booking'),
 ]
