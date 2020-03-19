d4362f3dfd509a103428c5dcd037298f26e453f0
Marcel <marceli.lagoda@gmail.com>
2020-03-19 01:02:25 +0100

    Add methods to db models

--- bookings/models.py
+++ bookings/models.py
@@ -1,5 +1,6 @@
 from django.contrib.auth import get_user_model
 from django.db import models
+from django.urls import reverse
 
 
 class Booking(models.Model):
@@ -17,3 +18,9 @@ class Booking(models.Model):
     no_of_people = models.PositiveSmallIntegerField(default=1)
     status = models.CharField(max_length=16, choices=STATUS_CHOICE)
     user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, related_name='users')
+
+    def __str__(self):
+        return f'{self.number}'
+
+    def get_absolute_url(self):
+        return reverse('booking', args=[str(self.id)])
